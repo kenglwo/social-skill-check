341535c413af17f3230857d580d8e0c5699a6df8
Kento <kentoglwo@gmail.com>
2024-05-28 16:17:31 +0900

    :sparkles: add material ui and grid layout

--- frontend/app/app/interface/page.tsx
+++ frontend/app/app/interface/page.tsx
@@ -1,3 +1,17 @@
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
 export default function Test() {
-  return (<h1>Hello</h1>)
+  return (
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={1} />
+        <Grid xs={10}>
+          <div>Content here</div>
+        </Grid>
+        <Grid xs={1} />
+      </Grid>
+    </Box>
+  )
 }
