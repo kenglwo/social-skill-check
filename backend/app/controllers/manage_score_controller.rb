c0b881b6ac9db5c146bbbc4616e164eb6081eecb
kentoglwo@gmail.com <kentoglwo@gmail.com>
2024-07-06 12:05:57 +0800

    :sparkles: save score data to db

--- backend/app/controllers/manage_score_controller.rb
+++ backend/app/controllers/manage_score_controller.rb
@@ -3,22 +3,36 @@ class ManageScoreController < ApplicationController
 
         user_id = params['user_id']
         score_data = params['score_data_all']
+        # score_data_hash = JSON.parse(score_data)
+
+
+        # score_data_hash.each do |key, value|
+        score_data.each do |ability_key, ability_value|
+
+            ability_value.each do |score_key, score_value|
+                score_key_components = score_key.split("_")
+                # logger.debug score_key_components
+                
+                ability_num = score_key_components[0][-1].to_i
+                category_num = score_key_components[1][-1].to_i
+                question_num = score_key_components[2][-1].to_i
+                recorded_at = score_key_components[3].chomp("m").to_i
+
+                # logger.debug "#{ability_num}_#{category_num}_#{question_num}_#{recorded_at}"
+
+                ScoreDatum.create(
+                    user_id: user_id,
+                    ability_num: ability_num,
+                    category_num: category_num,
+                    question_num: question_num,
+                    recorded_at: recorded_at,
+                    score: score_value
+                )
+            end
+        end
 
         # return calculated scores
-        api_result = {
-            "ability1_category1": 0,
-            "ability1_category2": 0,
-            "ability1_category3": 0,
-            "ability2_category1": 0,
-            "ability2_category2": 0,
-            "ability2_category3": 0,
-            "ability3_category1": 0,
-            "ability3_category2": 0,
-            "ability3_category3": 0,
-            "ability3_category4": 0,
-            "ability3_category5": 0,
-            "ability3_category6": 0,
-        }
+        api_result = {"status": "OK"}
 
         render json: api_result
     end
