78ad9320d916718314ee23f2eea0112c97f89189
kentoglwo@gmail.com <kentoglwo@gmail.com>
2024-07-06 11:49:03 +0800

    :recycle: add question_num to ScoreData

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_05_134816) do
+ActiveRecord::Schema.define(version: 2024_07_06_034513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2024_07_05_134816) do
     t.integer "recorded_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "question_num"
   end
 
 end
